76a4c7fa7b87cc0a97de04c73133e456dccf4571
ayethandar <ayethandar@abank.com.mm>
2024-06-26 16:43:35 +0630

    setup gh-pages

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -9,11 +9,13 @@ const Works = lazy(() => import("../pages/works/Works"));
 const Login = lazy(() => import("../pages/login/Login"));
 const Contact = lazy(() => import("../pages/contact/Contact"));
 const Brands = lazy(() => import("../pages/brand/brand"));
+const Error = lazy(() => import("../pages/error/Error"));
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <PublicRoute />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/home", element: <HomePage /> },
---
+++ src/pages/error/Error.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Error.module.css";
+
+const Error = () => {
+  return <div className={styles.errorPage}>Error</div>;
+};
+
+export default Error;
